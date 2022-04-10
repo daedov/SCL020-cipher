19227adc0c766e73a3f789081a0081deeb29db98
daedov <daniqanil@gmail.com>
2022-04-09 22:50:16 -0400

    cipher.js e index.js revisados

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,35 @@
-//métodos: cipher.encode (offset right, string) y cipher.decode (offset left, string)
-
-//function nameFunction(param1, param2){} los parametros son usados para defirnir una función
-
-//implementar objeto cipher
+//implementación del objeto cipher
 const cipher = {
   
-  //función con 2 parámetros: desplazamiento y texto
+  //método para cifrar 
   encode: function (offset, string) {
     let messageCipher = ''; // nuevo mensaje ingresado
-    string = string.toUpperCase(); 
-    for (let i = 0; i < string.length; i++) {
-      //método string.length para conocer la longitud de la cadena
+    string = string.toUpperCase(); // convertir texto ingresado a mayúsculas 
+    // ciclo for para recorrer el texto ingresado para convertirlo a valor unicode y luego en cadena 
+    for (let i = 0; i < string.length; i++) { 
       let txtConvertAscii = string.charCodeAt(i); // devuelve el numero del valor unicode
-      if (txtConvertAscii >= 65 && txtConvertAscii <= 90) {
-        //rango de letras mayúsculas 65-90 en ascii
-        messageCipher += String.fromCharCode(
-          //devuelve la cadena creada de la secuencia de valores unicode
-          (txtConvertAscii - 65 + parseInt(offset)) % 26 + 65
-        ); //cálculo para cifrar
+      if (txtConvertAscii >= 65 && txtConvertAscii <= 90) { // rango de letras mayúsculas en ASCII
+        messageCipher += String.fromCharCode( //devuelve la cadena creada de la secuencia de valores unicode
+          (txtConvertAscii - 65 + parseInt(offset)) % 26 + 65 //cálculo para cifrar
+        ); 
       }
     }
-    return messageCipher; // mensaje cifrado
+    return messageCipher; // retorna mensaje cifrado
   },
-  
+
+  //método para descifrar   
   decode: function (offset, string) {
     let messageCipher = ''; // nuevo mensaje ingresado
     string = string.toUpperCase(); 
     for (let i = 0; i < string.length; i++) {
-      //método string.length para conocer la longitud de una cadena
-      let txtConvertAscii = string.charCodeAt(i); // convertir a unicodes
-      if (txtConvertAscii >= 65 && txtConvertAscii <= 90) {
-        //rango de letras mayúsculas 65-90 en ascii
-        messageCipher += String.fromCharCode(
-          (txtConvertAscii + 65 - parseInt(offset)) % 26 + 65
-        ); //cálculo para descifrar???
+      let txtConvertAscii = string.charCodeAt(i); // devuelve el numero del valor unicode
+      if (txtConvertAscii >= 65 && txtConvertAscii <= 90) { //rango de letras mayúsculas en ASCII
+        messageCipher += String.fromCharCode( //devuelve la cadena creada de la secuencia de valores unicode
+          (txtConvertAscii + 65 - parseInt(offset)) % 26 + 65 //cálculo para descifrar 
+        ); 
       }
     }
-    return messageCipher; //mensaje descifrado
+    return messageCipher; // retorna mensaje descifrado
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-//escuchar eventos del DOM
-//invocar cipher.encode y cipher.decode
-//actualizar resultado en textarea
 import cipher from './cipher.js';
 
-//declaración de variables 
+//declaración de variables - manipulación del DOM desde el #id
 let offsetCipher = document.getElementById('num-cipher'); //desplazamiento del cifrado
-
 let messageInput = document.getElementById('msn-input'); //mensaje ingresado
-
 let messageOutput = document.getElementById('msn-output'); //mensaje de salida
 
+const btnEncode = document.getElementById('btnEncode'); //botón para cifrar
+const btnDecode = document.getElementById('btnDecode'); //botón para descifrar
+const btnClear = document.getElementById('btnClear'); //botón limpiar mensaje
+const btnLink = document.getElementById('btnLink'); //botón enviar mensaje - simula envío 
 
-const btnEncode = document.getElementById('btnEncode'); //botón cifrar
-const btnDecode = document.getElementById('btnDecode'); //botón descrar
-//const btnLink = document.getElementById('btnLink'); botón enviar ...simular envío 
-
+//invocar cipher.encode
 btnEncode.addEventListener('click', function(){
     let messageCipher = cipher.encode(offsetCipher.value, messageInput.value); 
     messageOutput.value = messageCipher; 
 });
 
+//invocar cipher.decode
 btnDecode.addEventListener('click', function(){
     let messageCipher = cipher.decode(offsetCipher.value, messageInput.value); 
     messageOutput.value = messageCipher; 
 });
 
-/*function clear(){
-    document.getElementById('offsetCipher').value = ''; 
-    document.getElementById('messageInput').value = ''; 
-    document.getElementById('messageOutput').value = ''; 
-}*/
+//función para limpiar los campos 
+btnClear.addEventListener('click', function() {
+    document.getElementById('offsetCipher').offsetCipher.value = '' ;
+    document.getElementById('messageInput').messageInput.value = '';
+    document.getElementById('messageOutput').messageOutput.value = '';
+    alert('¡Ingresa un nuevo mensaje!');
+})
 
-
+//función para simular envío del mensaje
+btnLink.addEventListener('click', function() {
+    alert('¡Tu mensaje ha sido enviado!');
+})
 
 
 
